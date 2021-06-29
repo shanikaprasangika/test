1b7c7172a3a2babdde828ec4775763bd5f3bec3e
shanikaprasangika <36036835+shanikaprasangika@users.noreply.github.com>
2021-06-29 18:26:11 +0530

    Update emp_position_create_test.js

--- emp_position_create_test.js
+++ emp_position_create_test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import configureStore from 'redux-mock-store';
 import renderer from 'react-test-renderer';
-import empPositionCreateForm from 'src/views/emp/EmpPositionCreateForm';
+import empPositionCreateForm from 'src/views/admin/EmpPositionCreateForm';
 
 const mockStore = configureStore([]);
 
